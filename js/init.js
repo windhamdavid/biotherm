e07eda86507dab4875530272b3ec5cd537c08e33
windhamdavid <david@davidawindham.com>
2018-06-21 09:07:33 -0400

    slow down slider

--- js/init.js
+++ js/init.js
@@ -16,3 +16,8 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
 	event.preventDefault();
 	$(this).ekkoLightbox();
 });
+
+
+$('#front-carousel').carousel({
+    interval: 8000
+});
